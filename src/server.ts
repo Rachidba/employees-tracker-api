2076cb8dd0ed55907058fdc8cab066592e5cb942
rachidba <rachidbaaziz.contact@gmail.com>
2020-05-28 02:07:54 +0000

    Move employee crud operations to controller

--- src/server.ts
+++ src/server.ts
@@ -1,53 +1,46 @@
-//server.js
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
 
-// BASE SETUP
-// ===========================================================================
+const app = express();
 
-// call the packages we need
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var morgan = require('morgan');
-
-// Configure app to use bodyParser()
-// This will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
-var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
-var config = require('../config'); // get our config file
+import jwt from 'jsonwebtoken';
 
-var mongoose = require('mongoose');
+import config from '../config';
+
+import mongoose from 'mongoose';
 mongoose.connect(config.database);
 
-var Employee = require('./models/employee');
-var Location = require('./models/location'); 
-var User     = require('./models/user')   
+import { Employee } from './models/employee';
+import { Location } from './models/location';
+import { User  } from './models/user'; 
 
-var port = process.env.PORT || 8085; //set our port
+import { EmployeeController } from './controllers/employees';
 
-app.set('superSecret', config.secret); // secret variable
+const port = process.env.PORT || 8085;
 
-// use morgan to log requests to the console
+app.set('superSecret', config.secret);
+
 app.use(morgan('dev'));
 
 // ROUTES FOR OUR API
 // ================================================================================
 
-var router = express.Router();  // get instance of express router
+const router = express.Router();
 
-// basic route
+
 router.get('/', function(req, res) {
     res.json({message: 'Welcom to our API!'});
 });
 
-// route to authenticate a user (POST http://localhost:8085/api/authenticate)
 router.post('/authenticate', function(req, res) {
-    // find the user
     User.findOne({
         name: req.body.name
-    }, function(err, user) {
-  
+    },
+    function(err, user) {
         if (err) throw err;
   
         if (!user) {
@@ -65,7 +58,7 @@ router.post('/authenticate', function(req, res) {
             const payload = {
                 admin: user.admin 
             };
-            var token = jwt.sign(payload, app.get('superSecret'), {
+            const token = jwt.sign(payload, app.get('superSecret'), {
                 expiresIn: '1440m'  // expires in 24 hours
             });
   
@@ -81,14 +74,12 @@ router.post('/authenticate', function(req, res) {
 });
 
 
-// route that will create a user 
 router.get('/setup', function(req, res) {
-    // Create a sample user
-    var admin = new User({
+    const admin = new User({
         name: 'admin',
         password: 'password',
         admin: true
-    }); 
+    });
 
     // Save the sample user
     admin.save(function(err) {
@@ -101,11 +92,11 @@ router.get('/setup', function(req, res) {
 // route middleware to verify a token
 router.use(function(req, res, next) {
     // check header or url parameters or post parameters for token
-    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+    const token = req.body.token || req.query.token || req.headers['x-access-token'];
     // decode token
     if (token) {
         // verifies secret and checks exp
-        jwt.verify(token, app.get('superSecret'), function(err, decoded) {      
+        jwt.verify(token, app.get('superSecret'), function(err: any, decoded: any) {      
             if (err) {
                 return res.json({ success: false, message: 'Failed to authenticate token.' });    
             } else {
@@ -130,84 +121,25 @@ router.use(function(req, res, next) {
 
 // on routes that end in /employees
 // -------------------------------------------------------------------------
+const employeeController = new EmployeeController();
+
 router.route('/employees')
-    //Create an employee
-    .post(function(req, res) {
-        var employee = new Employee();
-        employee.fname = req.body.fname;
-        employee.lname = req.body.lname;
-        employee.img_url = req.body.img_url;
-        employee.zone = req.body.zone;
+    .post(employeeController.createEmployee)
+    .get(employeeController.getEmployees);
 
-        // Save employee and check for errors
-        employee.save(function(err) {
-            if (err) {
-                res.send(err);
-            } 
-            res.json({message: 'Employee created !'});
-        })
-    })
-    // Get all employees
-    .get(function(req, res) {
-        Employee.find(function(err, employees) {
-            if (err) {
-                res.send(err);
-            }
-            res.json(employees);
-        });
-    });
-// on routes that end in /employees/:employee_id
+// on routes that end in /employees/:employeeId
 // ----------------------------------------------------
-router.route('/employees/:employee_id')
-
-    // get the employee with that id (accessed at GET http://localhost:8085/api/employees/:employee_id)
-    .get(function(req, res) {
-        Employee.findById(req.params.employee_id, function(err, employee) {
-            if (err)
-                res.send(err);
-            res.json(employee);
-        });
-    })
-    // update the employee with this id (accessed at PUT http://localhost:8085/api/employees/:employee_id)
-    .put(function(req, res) {
-
-        // use our bear model to find the bear we want
-        Employee.findById(req.params.employee_id, function(err, employee) {
-
-            if (err)
-                res.send(err);
-
-            employee.fname = req.body.fname;  // update the employee info
-
-            // save the empoyee
-            bear.save(function(err) {
-                if (err)
-                    res.send(err);
-
-                res.json({ message: 'Employee updated!' });
-            });
-
-        });
-    })
-    // delete the employee with this id (accessed at DELETE http://localhost:8080/api/employees/:employee_id)
-    .delete(function(req, res) {
-        Employee.remove({
-            _id: req.params.employee_id
-        }, function(err, employee) {
-            if (err)
-                res.send(err);
-
-            res.json({ message: 'Successfully deleted' });
-        });
-    });
+router.route('/employees/:employeeId')
+    .get(employeeController.getEmployee)
+    .put(employeeController.updateEmployee)
+    .delete(employeeController.deleteEmployee);
 
 // on routes that end in /locations
 // -------------------------------------------------------------------------------------
 router.route('/locations')
-    //Create a location
     .post(function(req, res) {
-        var location = new Locations();
-        location.employee_id = req.body.fname;
+        const location = new Location();
+        location.employeeId = req.body.fname;
         location.zone = req.body.zone;
         location.time = req.body.time;
 
