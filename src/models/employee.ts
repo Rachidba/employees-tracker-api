c44be542160edf79d56fcbd02e5b68b54631b793
rachidba <rachidbaaziz.contact@gmail.com>
2020-06-05 00:42:26 +0100

    Update employee model

--- src/models/employee.ts
+++ src/models/employee.ts
@@ -1,21 +1,28 @@
 import  { Schema, Document, model, Model} from 'mongoose';
 
 const EmployeeSchema: Schema = new Schema({
+    employeeId: {
+        type: String,
+        unique: true,
+        required: true
+    },
     firstName:{
         type: String
     },
     lastName: {
         type: String,
     },
-    imgUrl: String,
-    zone:  Number
+    originAreaId: {
+        type: Number,
+        required: true
+    }  
 });
 
 export interface IEmployee extends Document {
+    employeeiD: string,
     firstName: string;
     lastName: string;
-    imgUrl: string;
-    zone: number;
+    originAreaId: number;
 }
 
 export const Employee: Model<IEmployee> = model<IEmployee>('Employee', EmployeeSchema);
